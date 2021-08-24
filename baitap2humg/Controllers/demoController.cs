f1c2959ae4383815abcaa34b2a706331b1d8549f
min <min@DESKTOP-T2NAVI1>
2021-08-24 14:24:47 +0700

    Add project files.

---
+++ baitap2humg/Controllers/demoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using baitap2humg.Models;
+
+namespace baitap2humg.Controllers
+{
+    public class demoController : Controller
+    {
+        giaiphuongtrinh gpt = new giaiphuongtrinh();
+        // GET: demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult hello()
+        {
+            return View();
+        }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double hesoa, double heosob)
+        {
+            double x = gpt.giaiphuongtrinhbacnhat(hesoa, heosob);
+            ViewBag.nghiemPT = x;
+            return View();
+        }
+    }
+}
---
+++ baitap2humg/Models/giaiphuongtrinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap2humg.Models
+{
+    public class giaiphuongtrinh
+    {
+        public double giaiphuongtrinhbacnhat(double a, double b)
+        {
+            double x;
+            x = -b / a;
+            return x;
+        }
+    }
+}
